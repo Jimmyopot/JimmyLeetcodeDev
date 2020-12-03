42f1a2cfe434db1a0fe856dc5c8e3003fa245a0c
Jimmy Opot <jimmyopot@gmail.com>
2020-12-03 15:14:43 +0300

    Binary file was added

---
+++ add_binary.py
@@ -0,0 +1,20 @@
+"""
+HOW BINARY SYSTEM WORKS:
+
+- Each position in a num rep a certain value/binary.
+- For every num, the position rep BASE 10, eg. 237
+
+2 1 0 -> power of....
+2 3 7
+(10^2 * 2) + (10^1 * 3) + (10^0 * 7) = 237
+    200           30            7
+    
+    
+- For binary digits, the position rep BASE 2, eg. 111
+
+2 1 0 -> power of....
+1 1 1
+(2^2 * 1) + (2^1 * 1) + (2^0 * 1) = 6
+    4            2           0
+
+"""
