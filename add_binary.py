46aa4ab07fb9dbe5dac813dbc9b5d352989247b0
Jimmy Opot <jimmyopot@gmail.com>
2020-12-07 11:04:56 +0300

    Added how to add binary code in python

--- add_binary.py
+++ add_binary.py
@@ -12,9 +12,39 @@ HOW BINARY SYSTEM WORKS:
     
 - For binary digits, the position rep BASE 2, eg. 111
 
-2 1 0 -> power of....
+4 2 1 -> power of....
 1 1 1
-(2^2 * 1) + (2^1 * 1) + (2^0 * 1) = 6
-    4            2           0
+(1 * 1) + (2 * 1) + (4 * 1) = 7
+   1         2         4
+   
+1 0 1 1 1 0 1 1
+(1 * 1) + (2 * 1) + (4 * 0) + (8 * 1) + (16 * 1) + (32 * 1) + (64 * 0) + (128 * 1) = 187
 
 """
+
+# soln 1
+
+class Solution(object):
+    def addBinary(self, a, b):
+        a_decimal = 0
+        b_decimal = 0
+        
+        a = a[::-1]  # this just means reverse list
+        b = b[::-1]
+        a = list(a)  # make a and b into a list
+        b = list(b)
+        i = 0
+        
+        while i < len(a):
+            if a[i] == "1":
+                a_decimal += 2**i
+            i += 1
+        while i < len(b):
+            if b[i] == "1":
+                b_decimal += 2**i
+            i += 1
+            
+        return bin(a_decimal + b_decimal)[2:]
+    
+l = Solution()
+print(l.addBinary("11", "1"))
