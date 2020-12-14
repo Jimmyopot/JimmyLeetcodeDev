d6fcf38f8dae83f9ffacd629a900a0ba77008657
Jimmy Opot <jimmyopot@gmail.com>
2020-12-14 15:46:47 +0300

    Added Easy folder for easy questions on Leetcode

---
+++ Easy/climbing_stairs.py
@@ -0,0 +1,14 @@
+'''
+DYNAMIC PROGRAMMING
+
+- All about RECURSION => REPETITION! 
+- Pattern based, eg, Fibonacci sequence.
+- Find a recursive soln, are there repeated states in it?, and store them in a matrix.
+  1. Recursion.
+  2. Store (Memoize).
+  3. Bottom-up.
+- You see trends in the soln.
+- eg, Fibonacci sequence:
+  1, 1, 2, 3, 5, 8, 13
+  (1+1=2), (1+2=3), (2+3=5), (3+5=8), (5+8=13)
+'''
